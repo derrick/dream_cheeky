b92bf22c9cc8887d3296d4f487687fa2fbc9a6c0
Matt Sephton <matt.sephton@gmail.com>
2012-09-25 21:00:20 +0100

    Add examples

---
+++ examples/big_red_button-browse.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'dream_cheeky'
+require 'watir-webdriver'
+
+DreamCheeky::BigRedButton.run do
+  browser = Watir::Browser.new
+
+  open do
+    browser.goto 'devastatingexplosions.com'
+  end
+
+  close do
+    browser.close
+  end
+
+  push do
+    browser.object(:id => 'flashcontent').click
+  end
+end
