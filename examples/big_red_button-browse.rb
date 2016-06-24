7214cc1cd2d678162d4a49aba46e73a7ec96cc37
Eric Betts <bettse@fastmail.fm>
2016-06-24 09:13:26 -0700

    Update browser example URL

--- examples/big_red_button-browse.rb
+++ examples/big_red_button-browse.rb
@@ -8,7 +8,7 @@ DreamCheeky::BigRedButton.run do
   open do
     ## to use google chrome instead of default firefox add :chrome after new (with a space) and add the ChromeDriver http://code.google.com/p/chromedriver/downloads/list to your $PATH
     browser = Watir::Browser.new
-    browser.goto 'devastatingexplosions.com'
+    browser.goto 'www.soulstice.org'
   end
 
   close do
